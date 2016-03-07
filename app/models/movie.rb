9de295f2538f2b76e99f92cbcbddc936fc4cd3dc
biglam <craig@techiteasy.org.uk>
2016-03-07 15:54:40 +0000

    added and tested retrieving data from omdb using httparty

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,2 +1,15 @@
 class Movie < ActiveRecord::Base
+
+	def get_data_from_omdb
+		omdb_details = HTTParty.get("http://www.omdbapi.com/?t=#{self.title}&y=&tomatoes=true&r=json")
+		
+		self.title = omdb_details['Title']
+		self.year = omdb_details['Year'].to_i
+		self.imdbranking = omdb_details['imdbRating']
+		self.tomatorating = omdb_details['tomatoRating']
+		self.takings = omdb_details['BoxOffice']
+
+		# binding.pry;''
+		# self.save
+	end
 end
